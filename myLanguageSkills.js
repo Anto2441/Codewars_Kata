db22814d30b5420f447de7b63cac31cb90815246
Anthony Desorbais <anthony@derniercri.io>
2025-04-23 10:14:38 +0200

    feat: add katas

---
+++ myLanguageSkills.js
@@ -0,0 +1,28 @@
+// You are given a dictionary-like object (implementation may vary by language) containing languages as keys and your corresponding test results as values. Return the list of languages where your score is at least 60, in descending order of the scores.
+
+// Note: the scores will always be unique (so no duplicate values)
+
+// Examples
+// Input object with key-value pairs:
+// "Java" -> 10, "Ruby" -> 80, "Python" -> 65
+// Ouput:
+// [ "Ruby", "Python" ]
+
+// Input object with key-value pairs:
+// "Hindi" -> 60, "Greek" -> 71, "Dutch" -> 93
+// Output:
+// [ "Dutch", "Greek", "Hindi" ]
+
+// Input object with key-value pairs:
+// "C++" -> 50, "ASM" -> 10, "Haskell" -> 20
+// Output:
+// [ ]
+
+function myLanguages(results) {
+  const arr = Object.entries(results);
+  const filteredArray = arr.filter(([lang, score]) => score >= 60);
+  const sortedFilteredArray = filteredArray.sort((a, b) => b[1] - a[1]);
+  const sortedLanguages = sortedFilteredArray.map((x) => x[0]);
+
+  return sortedLanguages;
+}
