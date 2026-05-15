2549cc107711024596fc42a33be412923b7d7e16
Anthony Desorbais <anthony@derniercri.io>
2026-05-15 15:50:28 +0200

    feat: 加入kata

---
+++ eliminateTheIntruders.js
@@ -0,0 +1,14 @@
+// Task
+// You are given a string representing a number in binary. Your task is to delete all the unset bits in this string and return the corresponding number (after keeping only the '1's).
+
+// Examples
+// "11010101010101" ->  255 (= 0b11111111)
+// "111" ->  7
+// "1000000" -> 1
+// "000" -> 0
+
+function eliminateTheIntruders(input) {
+  const onesOnly = input.split('0').join('');
+
+  return parseInt(onesOnly, 2);
+}
